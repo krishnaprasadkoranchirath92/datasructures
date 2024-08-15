41ec137d02fb899ebf9552a440f62c7ff2309bfe
KoranchirathKrishnaprasad <krishnaprasadkoranchirath92@gmail.com>
2024-08-15 23:27:09 +0530

    functions

--- src/main/java/com/dsa/aplha/datastructures/functions/CheckPrime.java
+++ src/main/java/com/dsa/aplha/datastructures/functions/CheckPrime.java
@@ -2,4 +2,19 @@ package com.dsa.aplha.datastructures.functions;
 
 public class CheckPrime {
 
+	public static void main(String[] args) {
+		int n = 2;
+		boolean p = false;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				p = true;
+				break;
+			}
+		}
+		if (p) {
+			System.out.println("Composite");
+		} else {
+			System.out.println("Prime");
+		}
+	}
 }
